9f1306afeaaed16d562d7a955e28a32a87ef9a7b
Gustavo R. Kuhn <gustavorichterkuhn@gmail.com>
2025-09-19 17:58:41 -0300

    Adicionados os recursos, com permissões e tipos do objeto 3410: Device Extension

---
+++ subsys/net/lib/lwm2m/ucifi_device_extension.h
@@ -0,0 +1,31 @@
+/**
+ * @file ucifi_lamp.h
+ * @brief Resource IDs for uCIFI Device Extension object (ID: 3410)
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __LWM2M_UCIFI_DEVICE_EXTENSION__
+#define __LWM2M_UCIFI_DEVICE_EXTENSION__
+
+#define UCIFI_OBJECT_DEVICE_EXTENSION_ID              3410
+
+/* uCIFI Device Extension(DE) resource IDs */
+#define UCIFI_DE_GTIN_MODEL_NUMBER_RID             1
+#define UCIFI_DE_MANUFACTURER_RID                  2
+#define UCIFI_DE_USER_GIVEN_NAME_RID               3
+#define UCIFI_DE_ASSET_ID_RID                      4
+#define UCIFI_DE_INSTALLATION_DATE_RID             5
+#define UCIFI_DE_SOFTWARE_UPDATE_RID               6
+#define UCIFI_DE_MAINTENANCE_RID                   7
+#define UCIFI_DE_CONFIG_RST_RID                    8
+#define UCIFI_DE_DEVICE_OPERATING_HOURS_RID        9
+#define UCIFI_DE_ADDITIONAL_FIRMWARE_INFO_RID      10
+#define UCIFI_DE_DST_START_RID                     11
+#define UCIFI_DE_DST_END_RID                       12
+#define UCIFI_DE_DST_OFFSET_RID                    13
+#define UCIFI_DE_UPTIME_RID                        14
+#define UCIFI_DE_RFD_DEVICE_RID                    15
+
+
+#endif /* __LWM2M_UCIFI_DEVICE_EXTENSION__ */
+
