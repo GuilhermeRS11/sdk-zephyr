229a7eb86f91b2452d3fe7f30bd9cefa0d22678c
Gustavo R. Kuhn <gustavorichterkuhn@gmail.com>
2025-09-21 21:08:46 -0300

    adicionado objeto UCIFI 3417 ao core

---
+++ subsys/net/lib/lwm2m/ucifi_luminaire_asset.h
@@ -0,0 +1,28 @@
+/**
+ * @file ucifi_lamp.h
+ * @brief Resource IDs for uCIFI Luminaire Asset object (ID: 3417)
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __LWM2M_UCIFI_LUMINAIRE_ASSET__
+#define __LWM2M_UCIFI_LUMINAIRE_ASSET__
+
+#define UCIFI_OBJECT_LA_ID              3417
+
+/* uCIFI Luminaire Asset resource IDs */
+#define UCIFI_LA_GTIN_RID                      	    1
+#define UCIFI_LA_YEAR_OF_MANUFACTURING_RID          2
+#define UCIFI_LA_WEEK_OF_MANUFACTURING_RID          3
+#define UCIFI_LA_NOMINAL_LIGHT_OUTPUT_RID           4
+#define UCIFI_LA_LIGHT_DISTRIBUTION_TYPE_RID        5
+#define UCIFI_LA_LUMINAIRE_COLOR_RID                6
+#define UCIFI_LA_NOMINAL_INPUT_POWER_RID            7
+#define UCIFI_LA_POWER_AT_MIN_DIM_RID               8
+#define UCIFI_LA_NOMINAL_MAX_AC_MAINS_VOLTAGE_RID   9
+#define UCIFI_LA_NOMINAL_MIN_AC_MAINS_VOLTAGE_RID   10
+#define UCIFI_LA_CRI_RID                            11
+#define UCIFI_LA_CCT_VALUE_RID                      12
+#define UCIFI_LA_LUMINAIRE_ID_RID                   13
+#define UCIFI_LA_LUMINAIRE_ID_NUMBER_RID            14
+
+#endif /* __LWM2M_UCIFI_LUMINAIRE_ASSET__ */
