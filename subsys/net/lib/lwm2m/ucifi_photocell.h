14ee9fa4bac3e29c76c26bf4201e4c70ce009985
Gustavo R. Kuhn <gustavorichterkuhn@gmail.com>
2025-09-26 10:10:31 -0300

    adicionado photocell (obj 3419) ao core

---
+++ subsys/net/lib/lwm2m/ucifi_photocell.h
@@ -0,0 +1,20 @@
+/**
+ * @file ucifi_photocell.h
+ * @brief Resource IDs for uCIFI Photocell object (ID: 3419)
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __LWM2M_UCIFI_PHOTOCELL__
+#define __LWM2M_UCIFI_PHOTOCELL__
+
+#define UCIFI_OBJECT_PHOTOCELL_ID              3419
+
+/* uCIFI Photocell resource IDs */
+#define UCIFI_PHOTOCELL_ON_LUX_LEVEL_RID            1
+#define UCIFI_PHOTOCELL_OFF_LUX_LEVEL_RID           2
+#define UCIFI_PHOTOCELL_STATUS_RID                  3
+#define UCIFI_PHOTOCELL_TIMESTAMP_RID               5518
+#define UCIFI_PHOTOCELL_FRACTIONAL_TIMESTAMP_RID    6050
+
+
+#endif /* __LWM2M_UCIFI_PHOTOCELL__ */
